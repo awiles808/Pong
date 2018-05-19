3a741cbdce49e0cad492380f75c147340e6756fe
awiles808 <awiles808@gmail.com>
2018-05-19 17:26:10 -0600

    Ball bouncing all over

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 var canvas;
 var canvasContext;
 var ballX = 50;
+var ballY = 50;
 var ballSpeedX = 10;
+var ballSpeedY = 4;
 
 
 window.onload = function() {
@@ -20,11 +22,19 @@ window.onload = function() {
 
 function moveEverything() {
     ballX = ballX + ballSpeedX;
+    ballY = ballY + ballSpeedY;
     if(ballX < 0) {
             ballSpeedX = -ballSpeedX;
     }
     if(ballX > canvas.width) {
             ballSpeedX = -ballSpeedX;
+
+          }
+    if(ballY < 0) {
+            ballSpeedY = -ballSpeedY;
+    }
+    if(ballY > canvas.height) {
+            ballSpeedY = -ballSpeedY;
 
           }
 }
@@ -35,11 +45,17 @@ function drawCanvas() {
     //this is player1 paddle
     colorRect(0,210,10,100,'white');
     //next line draws the ball
-    // colorRect(ballX,100,10,10,'white');
-    canvasContext.fillStyle = "green";
+    colorCirle(ballX, ballY, 10, 'white')
+
+}
+
+function colorCirle(centerX, centerY, radius, drawColor) {
+
+    canvasContext.fillStyle = "drawColor";
     canvasContext.beginPath();
-    canvasContext.arc(ballX, 100, 10, 0, Math.PI*2, true);
+    canvasContext.arc(centerX, centerY, radius, 0, Math.PI*2, true);
     canvasContext.fill();
+
 
 }
 
