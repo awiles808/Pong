9868a7192940e5b8987bf155a96742ed0bfb6c39
awiles808 <awiles808@gmail.com>
2018-05-19 15:54:00 -0600

    Ball moving backwards after hits right side

--- script.js
+++ script.js
@@ -1,29 +1,38 @@
 var canvas;
 var canvasContext;
 var ballX = 50;
-var ballY;
+var ballSpeedX = 5;
+
 
 
 window.onload = function() {
   // alert('Am I connetected?');
-  canvas = document.getElementById('pongCanvas');
-  canvasContext = canvas.getContext('2d');
+    canvas = document.getElementById('pongCanvas');
+    canvasContext = canvas.getContext('2d');
 
-  setInterval(drawCanvas, 50);
-  drawCanvas();
-  drawCanvas();
-  drawCanvas();
+    var framesPerSecond = 30
+    setInterval(function() {
+          moveEverything();
+          drawCanvas();
+    }, 1000/framesPerSecond);
 }
-function drawCanvas(){
-ballX = ballX + 5;
 
-  console.log(ballX);
-  canvasContext.fillStyle = 'black';
-  canvasContext.fillRect(0,0,canvas.width,canvas.height);
-  canvasContext.fillStyle = 'green';
-  canvasContext.fillRect(225,210,200,200);
-  canvasContext.fillStyle = 'white';
-  canvasContext.fillRect(ballX,100,10,10);
+
+function moveEverything() {
+    ballX = ballX + ballSpeedX;
+    if(ballX > 800) {
+            ballSpeedX = -5;
+    }
+
+}
+
+function drawCanvas(){
+    canvasContext.fillStyle = 'black';
+    canvasContext.fillRect(0,0,canvas.width,canvas.height);
+    canvasContext.fillStyle = 'green';
+    canvasContext.fillRect(0,210,10,100);
+    canvasContext.fillStyle = 'white';
+    canvasContext.fillRect(ballX,100,10,10);
 
 
 }
