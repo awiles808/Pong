6171f985069a6a47f103f22ef5f7dc297b0ff959
awiles808 <awiles808@gmail.com>
2018-05-19 15:07:48 -0600

    I have motion

--- script.js
+++ script.js
@@ -1,24 +1,29 @@
 var canvas;
 var canvasContext;
-
+var ballX = 50;
+var ballY;
 
 
 window.onload = function() {
   // alert('Am I connetected?');
   canvas = document.getElementById('pongCanvas');
   canvasContext = canvas.getContext('2d');
+
+  setInterval(drawCanvas, 50);
   drawCanvas();
   drawCanvas();
   drawCanvas();
 }
 function drawCanvas(){
-  console.log('called drawCanvas()?!');
+ballX = ballX + 5;
+
+  console.log(ballX);
   canvasContext.fillStyle = 'black';
   canvasContext.fillRect(0,0,canvas.width,canvas.height);
   canvasContext.fillStyle = 'green';
   canvasContext.fillRect(225,210,200,200);
   canvasContext.fillStyle = 'white';
-  canvasContext.fillRect(canvas.width/2,200,50,25);
+  canvasContext.fillRect(ballX,100,10,10);
 
 
 }
